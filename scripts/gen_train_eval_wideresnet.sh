1ed45700f2bfa1f4e4a8480f6a4050f7a54f8c58
sunx <1059363093@qq.com>
2020-06-30 21:02:06 +0800

    ..

--- scripts/gen_train_eval_wideresnet.sh
+++ scripts/gen_train_eval_wideresnet.sh
@@ -6,15 +6,15 @@ for i in "CIFAR10 ${MODEL_NAME}_cifar10 1"; do
   read dataset model weight <<< "${i}";
 
   # 1. generate hieararchy
-  nbdt-hierarchy  --dataset=${dataset} --arch=${model}
+  nbdt-hierarchy  --dataset=${dataset} --arch=${model} --method=induced2
 
   # 2. train with soft tree supervision loss
-  python main.py --lr=0.01 --dataset=${dataset} --arch=${model} --hierarchy=induced-${model} --pretrained --loss=SoftTreeSupLoss --tree-supervision-weight=${weight}
+  python main.py --lr=0.01 --dataset=${dataset} --arch=${model} --hierarchy=induced2-${model} --pretrained --loss=SoftTreeSupLoss --tree-supervision-weight=${weight}
   # --lr=0.01 --dataset=CIFAR10 --arch=wrn28_10_cifar10 --hierarchy=induced-wrn28_10_cifar10 --pretrained --loss=SoftTreeSupLoss --tree-supervision-weight=1
 
   # 3. evaluate with soft then hard inference
   for analysis in SoftEmbeddedDecisionRules HardEmbeddedDecisionRules; do
-    python main.py --dataset=${dataset} --arch=${model} --hierarchy=induced-${model} --loss=SoftTreeSupLoss --eval --resume --analysis=${analysis} --tree-supervision-weight=${weight}
+    python main.py --dataset=${dataset} --arch=${model} --hierarchy=induced2-${model} --loss=SoftTreeSupLoss --eval --resume --analysis=${analysis} --tree-supervision-weight=${weight}
     # --dataset=CIFAR10 --arch=wrn28_10_cifar10 --hierarchy=induced-wrn28_10_cifar10 --loss=SoftTreeSupLoss --eval --resume --analysis=SoftEmbeddedDecisionRules --tree-supervision-weight=1
   done
 done;
